8920f894aa56e39d1f85b39bce57bfa781595db5
Ociel Lerma <oglerma@gmail.com>
2021-07-29 21:26:19 -0700

    Modifying Api class and PostList View to handle the observable object. TODO use StateObject instead of ObservedObject when instantiating

--- DesignCodeProject/Data.swift
+++ DesignCodeProject/Data.swift
@@ -18,7 +18,9 @@ class Api {
         guard let url = URL(string: "https://jsonplaceholder.typicode.com/posts") else { return }
         
         URLSession.shared.dataTask(with: url) { (data, _, _) in
-            let posts = try! JSONDecoder().decode([Post].self, from: data!)
+            guard let data = data else { return }
+            
+            let posts = try! JSONDecoder().decode([Post].self, from: data)
             
             DispatchQueue.main.async {
                 completion(posts)
