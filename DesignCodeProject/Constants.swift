4a14c868d5c4c44dd87350f8892be10fe2fa6c15
Ociel Lerma <oglerma@gmail.com>
2021-07-30 09:00:32 -0700

    adding contentful client fetch bases on content model built on their platform and added constant file

---
+++ DesignCodeProject/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  DesignCodeProject
+//
+//  Created by Ociel Lerma on 7/30/21.
+//
+
+import Foundation
+
+let spaceId = "API_KEY_HERE"
+let accessToken = "API_KEY_HERE"
---
+++ DesignCodeProject/CourseStore.swift
@@ -0,0 +1,20 @@
+//
+//  CourseStore.swift
+//  DesignCodeProject
+//
+//  Created by Ociel Lerma on 7/30/21.
+//
+
+import SwiftUI
+import Contentful
+
+
+let client = Client(spaceId: spaceId, accessToken: accessToken)
+
+func getArray() {
+    let query = Query.where(contentTypeId: "course")
+    
+    client.fetchArray(of: Entry.self, matching: query) { result in
+        print(result)
+    }
+}
