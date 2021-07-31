c7380a4ec89a5910aabd7c10112c12bf47ab411e
Ociel Lerma <oglerma@gmail.com>
2021-07-30 21:17:14 -0700

    handling response from services

--- DesignCodeProject/CourseStore.swift
+++ DesignCodeProject/CourseStore.swift
@@ -6,15 +6,40 @@
 //
 
 import SwiftUI
+import Combine
 import Contentful
-
 
 let client = Client(spaceId: spaceId, accessToken: accessToken)
 
-func getArray() {
-    let query = Query.where(contentTypeId: "course")
+func getArray(id: String, completion: @escaping([Entry]) -> ()) {
+    let query = Query.where(contentTypeId: id)
     
     client.fetchArray(of: Entry.self, matching: query) { result in
-        print(result)
+        switch result {
+        case .success(let array):
+            DispatchQueue.main.async {
+                completion(array.items)
+            }
+        case .failure(let error):
+            print(error)
+        }
+    }
+}
+
+class CourseStore: ObservableObject {
+    @Published var courses: [Course] = courseData
+    
+    init() {
+        getArray(id: "course") { (items) in
+            items.forEach { (item) in
+                self.courses.append(Course(
+                    title: item.fields["title"] as! String,
+                    subtitle: item.fields["subtitle"] as! String,
+                    image: #imageLiteral(resourceName: "Background1"),
+                    logo: #imageLiteral(resourceName: "Logo1"),
+                    color: #colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1),
+                    show: false))
+            }
+        }
     }
 }
