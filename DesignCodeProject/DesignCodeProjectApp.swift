ad7c2ff878718ffbb353fa2824cdb121fac0fdaf
Ociel Lerma <oglerma@gmail.com>
2021-12-24 11:05:57 -0800

    Adding Firebase package manager

--- DesignCodeProject/DesignCodeProjectApp.swift
+++ DesignCodeProject/DesignCodeProjectApp.swift
@@ -6,12 +6,21 @@
 //
 
 import SwiftUI
+import Firebase
+
+final class AppDelegate: NSObject, UIApplicationDelegate {
+	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+		FirebaseApp.configure()
+		return true
+	}
+}
 
 @main
 struct DesignCodeProjectApp: App {
+	@UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     var body: some Scene {
         WindowGroup {
-            TabBar()
+			LoginView()
         }
     }
 }
